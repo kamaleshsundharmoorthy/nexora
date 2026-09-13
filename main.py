7b6c1ce6c89d32cd027190e84d6adb8695380670
kamaleshsundharmoorthy <kamaleshsundharmoorthy@gmail.com>
2026-09-13 13:04:00 +0530

    domain add

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ allow_origins=[
 "http://localhost:3000",
 "http://127.0.0.1:3000",
 "https://nexora-kamal-97e6.vercel.app",
-"https://nexora-kamal.vercel.app/"
+"https://nexora-kamal.vercel.app"
 ],
 allow_credentials=True,
 allow_methods=["*"],
